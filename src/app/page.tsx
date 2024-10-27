514422b514df83aa87a7f196820476186d4f0cc0
waleed-saddique <wbs90704@gmail.com>
2024-10-27 22:59:00 +0500

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+"use client"
+import HeroSection from "../components/HeroSection";
+import Whychooseus from "@/components/whychooseus";
+import FeaturedWebinars from "@/components/FeaturedWebinars";
+import FeaturedCourses from "@/components/FeaturedCourses";
+import react from "react"
+
+const page = () => {
+  return (
+    <div>
+     <HeroSection/>
+     <FeaturedCourses/>
+  <Whychooseus/>
+  <FeaturedWebinars/>
+ 
+    </div>
+  )
+}
+
+export default page
